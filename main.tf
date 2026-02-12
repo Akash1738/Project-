117b95aba431d77140ad25713213dd3d5918038e
@kki... <akashbajgire12@gmail.com>
2026-02-12 11:57:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "simple_ec2" {
+resource "aws_instance" "simple-ec2" {
   ami           = "ami-019715e0d74f695be"   # Ubuntu 24.04 AMI in ap-south-1
   instance_type = "t3.micro"                # small instance
   key_name      = "k8s"                     # must exist in AWS
@@ -7,6 +7,6 @@ resource "aws_instance" "simple_ec2" {
   security_groups = ["sg-06fd543363e8e8d67"]
 
   tags = {
-    Name = "SimpleEC2"
+    Name = "Simple-EC2"
   }
 }
