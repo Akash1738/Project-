a014cac8e03c4af50cebcbc105ccdc8c644be68e
@kki... <akashbajgire12@gmail.com>
2026-02-12 11:44:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "terraform-practice" {
   ami           = "ami-019715e0d74f695be"   # Ubuntu 24.04 AMI in ap-south-1 (update if needed)
   instance_type = "t3.micro"                # At least 2GB RAM for Minikube
-  key_name      = k8s
-  security_groups = [aws_security_group.allow_ssh.name]
+  key_name      = "k8s"
+  security_groups = [sg-06fd543363e8e8d67]
 
   tags = {
     Name = "MyTerraformEC2"
